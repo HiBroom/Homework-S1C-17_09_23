0518dd502530d28bdb123347be340de8b1fa7de2
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-09-19 09:46:33 +0300

    Solved Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+int n = Convert.ToInt32(Console.ReadLine());
+if(n % 2 == 0)
+{
+    Console.WriteLine($"{n} -> Number divides into two");
+}
+else
+{
+    Console.WriteLine($"{n} -> Number doesn't divides into two ");
+}
